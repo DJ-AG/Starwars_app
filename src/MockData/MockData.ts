059c7f7252833086a2c51166db5b24fab21c197a
Alexei G <alekseigolovanov89@gmail.com>
2024-02-06 19:02:42 +0200

    Updated structure of the app

---
+++ src/MockData/MockData.ts
@@ -0,0 +1,132 @@
+// Remove after connecting to the API
+
+export const characters = [
+  {
+    id: '1',
+    name: 'Luke Skywalker',
+    image: 'https://example.com/luke.jpg',
+    bornLocation: 'Tatooine',
+    diedLocation: '',
+    homeworld: 'Tatooine',
+    affiliations: ['Rebel Alliance'],
+    apprentices: ['Ben Solo'],
+    gender: 'Male',
+    dimensions: 1.72,
+    species: 'Human',
+    wiki: 'https://starwars.fandom.com/wiki/Luke_Skywalker'
+  },
+  {
+    id: '2',
+    name: 'Darth Vader',
+    image: 'https://example.com/vader.jpg',
+    bornLocation: 'Tatooine',
+    diedLocation: 'Death Star II',
+    homeworld: 'Tatooine',
+    affiliations: ['Sith', 'Galactic Empire'],
+    apprentices: ['Ahsoka Tano'],
+    gender: 'Male',
+    dimensions: 2.02,
+    species: 'Human',
+    wiki: 'https://starwars.fandom.com/wiki/Darth_Vader'
+  },
+  {
+    id: '11',
+    name: 'Obi-Wan Kenobi',
+    image: 'https://example.com/obi-wan.jpg',
+    homeworld: 'Stewjon',
+    affiliations: ['Jedi Order'],
+    gender: 'Male',
+    species: 'Human',
+    wiki: 'https://starwars.fandom.com/wiki/Obi-Wan_Kenobi'
+  },
+  {
+    id: '12',
+    name: 'Mace Windu',
+    image: 'https://example.com/mace-windu.jpg',
+    homeworld: 'Haruun Kal',
+    affiliations: ['Jedi Order'],
+    gender: 'Male',
+    species: 'Human',
+    wiki: 'https://starwars.fandom.com/wiki/Mace_Windu'
+  },
+  {
+    id: '13',
+    name: 'Qui-Gon Jinn',
+    image: 'https://example.com/qui-gon.jpg',
+    homeworld: 'Coruscant',
+    affiliations: ['Jedi Order'],
+    gender: 'Male',
+    species: 'Human',
+    wiki: 'https://starwars.fandom.com/wiki/Qui-Gon_Jinn'
+  },
+  {
+    id: '14',
+    name: 'Padmé Amidala',
+    image: 'https://example.com/padme.jpg',
+    homeworld: 'Naboo',
+    affiliations: ['Galactic Republic'],
+    gender: 'Female',
+    species: 'Human',
+    wiki: 'https://starwars.fandom.com/wiki/Padm%C3%A9_Amidala'
+  },
+  {
+    id: '15',
+    name: 'Count Dooku',
+    image: 'https://example.com/dooku.jpg',
+    homeworld: 'Serenno',
+    affiliations: ['Sith'],
+    gender: 'Male',
+    species: 'Human',
+    wiki: 'https://starwars.fandom.com/wiki/Dooku'
+  },
+  {
+    id: '16',
+    name: 'Jango Fett',
+    image: 'https://example.com/jango-fett.jpg',
+    homeworld: 'Concord Dawn',
+    affiliations: ['Mandalorian'],
+    gender: 'Male',
+    species: 'Human',
+    wiki: 'https://starwars.fandom.com/wiki/Jango_Fett'
+  },
+  {
+    id: '17',
+    name: 'Greedo',
+    image: 'https://example.com/greedo.jpg',
+    homeworld: 'Rodia',
+    affiliations: ['Bounty Hunters'],
+    gender: 'Male',
+    species: 'Rodian',
+    wiki: 'https://starwars.fandom.com/wiki/Greedo'
+  },
+  {
+    id: '18',
+    name: 'Lando Calrissian',
+    image: 'https://example.com/lando.jpg',
+    homeworld: 'Socorro',
+    affiliations: ['Rebel Alliance'],
+    gender: 'Male',
+    species: 'Human',
+    wiki: 'https://starwars.fandom.com/wiki/Lando_Calrissian'
+  },
+  {
+    id: '19',
+    name: 'BB-8',
+    image: 'https://example.com/bb8.jpg',
+    homeworld: 'Unknown',
+    affiliations: ['Resistance'],
+    gender: 'N/A',
+    species: 'Droid',
+    wiki: 'https://starwars.fandom.com/wiki/BB-8'
+  },
+  {
+    id: '20',
+    name: 'Rey',
+    image: 'https://example.com/rey.jpg',
+    homeworld: 'Jakku',
+    affiliations: ['Resistance'],
+    gender: 'Female',
+    species: 'Human',
+    wiki: 'https://starwars.fandom.com/wiki/Rey'
+  }
+];
