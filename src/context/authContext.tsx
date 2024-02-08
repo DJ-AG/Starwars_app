7fb679ceafc8bb4192fd30fe9d2e3603416694f5
Alexei G <alekseigolovanov89@gmail.com>
2024-02-08 20:19:30 +0200

    updated the authContext setTimeout becouse of Typescript NodeJs.timeout interpreting

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -3,13 +3,14 @@ import React, {
   useContext,
   useState,
   useEffect,
+  useRef,
   ReactNode
 } from 'react';
 import { AuthContextType } from '../types';
 
 // Create the context with an initial undefined value
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
-const TOKEN_EXPIRATION_TIME = 5 * 60 * 1000;
+const TOKEN_EXPIRATION_TIME = 5 * 60 * 1000; // 5 minutes in milliseconds
 
 // Hook for consuming the context in components
 export const useAuth = (): AuthContextType => {
@@ -27,17 +28,15 @@ interface AuthProviderProps {
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const tokenTimerRef = useRef<number>(); // useRef to store the token expiration timer ID
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     setIsAuthenticated(!!token);
-    if (token) {
-      // Check if the token is expired on app start
-      if (isTokenExpired()) {
-        logout();
-      } else {
-        startTokenTimer();
-      }
+    if (token && !isTokenExpired()) {
+      startTokenTimer();
+    } else {
+      logout();
     }
   }, []);
 
@@ -55,40 +54,30 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     localStorage.removeItem('token');
     localStorage.removeItem('tokenExpirationDate');
     setIsAuthenticated(false);
-    if (tokenTimer) clearTimeout(tokenTimer); // Here, clearTimeout expects a number
+    if (tokenTimerRef.current) clearTimeout(tokenTimerRef.current);
   };
 
-  // Function to check if the token is expired
   const isTokenExpired = () => {
     const expirationDate = localStorage.getItem('tokenExpirationDate');
     return expirationDate && new Date(expirationDate) <= new Date();
   };
 
-  // Function to start a timer that will check for token expiration
-  let tokenTimer: number;
   const startTokenTimer = () => {
-    // Calculate the remaining time until the token expires
     const expirationDate = localStorage.getItem('tokenExpirationDate');
     const remainingTime = expirationDate
       ? new Date(expirationDate).getTime() - new Date().getTime()
       : 0;
-    if (remainingTime > 0) {
-      // Cast the return value of setTimeout to `number`
-      tokenTimer = setTimeout(
-        refreshToken,
-        remainingTime - 60000
-      ) as unknown as number;
-    }
 
-    // Set a timer to refresh the token a bit before it expires
+    if (tokenTimerRef.current) clearTimeout(tokenTimerRef.current); // Clear any existing timer
+
     if (remainingTime > 0) {
-      tokenTimer = setTimeout(refreshToken, remainingTime - 60000); // Refresh 1 minute before expiration
+      tokenTimerRef.current = window.setTimeout(() => {
+        refreshToken();
+      }, remainingTime - 60000) as unknown as number; // Refresh 1 minute before expiration
     }
   };
 
-  // Function to refresh the token
   const refreshToken = () => {
-    // Since this is a mock, simply reset the token expiration time
     const tokenExpirationDate = new Date(
       new Date().getTime() + TOKEN_EXPIRATION_TIME
     ).toISOString();
